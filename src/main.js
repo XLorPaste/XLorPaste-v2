b09ffec2aea1ead5ddb9d964423414491a9a125d
X1 Carbon <yjl9903@vip.qq.com>
2020-02-15 20:30:42 +0800

    add: ga

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import copy from 'copy-to-clipboard';
+import VueAnalytics from 'vue-analytics';
 import './assets/common.css';
 import './plugins/element';
 import './plugins/hljs';
+
+Vue.use(VueAnalytics, {
+  id: 'UA-150361094-1',
+  router
+});
 
 Vue.config.productionTip = false;
 
